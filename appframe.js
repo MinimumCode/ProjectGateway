5abb95a9bec80858e7e6a0bd41e9cf46abd610f6
Ferdinand A. Mercado <fmercado@openit.com>
2017-08-23 15:32:56 +0800

    Dev: Send client info to server and save in a json file. Add initilization of json file

--- appframe.js
+++ appframe.js
@@ -1,12 +1,15 @@
+const os = require("os");
+var interfaces = os.networkInterfaces();
+
 module.exports = {
 
-    stringToMap: function(str){
+    stringToMap: function (str) {
         var result = {};
-        str.split('=').forEach(function(x){
+        str.split('=').forEach(function (x) {
             var arr = x.split('=');
             arr[1] && (result[arr[0]] = arr[1]);
         });
         return result;
-    }
-}
+    },
 
+}
--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const https = require('https');
 const fs = require('fs')
 const formidable = require('formidable')
 const os = require("os");
+var appframe = require("./appframe")
+var interfaces = os.networkInterfaces();
 
 
-/*Initialize server*/
+/*-----------------------Initialize server------------------------*/
 var credentials = {
     key: fs.readFileSync('./keys/rsa.private.key', 'utf8'),
     cert: fs.readFileSync('./keys/cert.pem', 'utf8')
@@ -23,6 +25,29 @@ httpsServer.listen(443, function () {
     console.log("Listening secure server " + os.hostname() + ":" + httpsServer.address().port);
 });
 
+/*gets local server ip*/
+var addresses = [];
+for (var interface in interfaces) {
+    for (var index in interfaces[interface]) {
+        var address = interfaces[interface][index];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+var json_file = __dirname + "/openit_client_info.json";
+if (! fs.existsSync(json_file)) {
+    fs.writeFile(json_file , "{}", { flag: 'wx' }, function (err) {
+        if (err) throw err;
+        console.log("Intialized openit_client_info.json");
+    });
+}
+
+
+
+/*-----------------------Initialize server------------------------*/
+   
+    
 
 
 /*process.argv.forEach(function (val, index, array) {
@@ -30,7 +55,83 @@ httpsServer.listen(443, function () {
 });*/
 
 
-app.get('/', function (req, res) {
+
+/*TODO: Execute a command using rest remotely*/
+
+/*TODO: update client information save to json file*/
+app.post('/clientinfo:?', function (req, res) {
+
+    /*
+        curl -i -X POST  "https://mnl199win/clientinfo?host=mnl192win&type=client&jobs=Flexlogcollector,licpoll&version=8.0.0.0&os=windows_10_enterprise&patches=1321,1322,1323" --cacert keys/cert.pem
+
+    */
+    var json_file = __dirname + "/openit_client_info.json"
+    fs.readFile(json_file, "utf8", function (err, data) {
+
+        var client = [];
+        client.hostname = req.query.host;
+
+        client.jobs = [];
+        if (req.query.jobs != null)
+            client.jobs = req.query.jobs.split(",");
+
+        client.patches = [];
+        if (req.query.patches != null)
+            client.patches = req.query.patches.split(",");
+
+        client.version = "unknown";
+        if (req.query.version != null)
+            client.version = req.query.version;
+
+        client.os = "unknown";
+        if (req.query.os != null)
+            client.os = req.query.os;
+
+        /*
+            version
+            arch
+            ip
+            hostname
+            patches
+            os
+        */
+        var server = [];
+        server.epoch = Math.round((new Date).getTime() / 1000);
+        server.date = new Date();
+
+
+
+        var clientinfo = {
+            "client": {
+                "jobs": client.jobs,
+                "version": client.version,
+                "arch": client.arch,
+                "ip": client.ip,
+                "hostname": client.hostname,
+                "patches": client.patches,
+                "os": client.os
+            },
+            "server": {
+                "epoch": server.epoch,
+                "date": server.date,
+                "ip": addresses[0],
+                "hostname": os.hostname()
+            }
+        };
+
+        var clients = {};
+        clients = JSON.parse(data);
+        clients[client.hostname] = clientinfo;
+
+        clients = JSON.stringify(clients);
+        fs.writeFile(json_file, clients, function (err) {
+            if (err) throw err;
+            console.log('updated', clientinfo);
+            res.end();
+        })
+    })
+})
+app.get('/ping', function (req, res) {
     res.send('hello');
 });
 
